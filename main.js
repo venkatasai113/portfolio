e08b6b6a95b37bc8e859ce34fdbd7d3eb92c45d4
venkatasai113 <venkatasaiattada123@gmail.com>
2024-08-10 23:41:27 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+/*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu'),
+      navToggle=document.getElementById('nav-toggle'),
+      navClose=document.getElementById('nav-close')
+
+
+if(navToggle)
+{
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose)
+    {
+        navClose.addEventListener('click',()=>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
+    
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink=document.querySelectorAll('.nav__link')
+
+const linkAction=()=>
+{
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+/*=============== SWIPER PROJECTS ===============*/
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
