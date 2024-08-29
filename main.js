4e718aacb06a07954d11e295ebb02ed4ef143428
venkatasai113 <venkatasaiattada123@gmail.com>
2024-08-29 16:17:19 +0530

    https://github.com/venkatasai113/portfolio.git

--- main.js
+++ main.js
@@ -38,6 +38,48 @@ navLink.forEach(n=>n.addEventListener('click',linkAction))
 
 /*=============== EMAIL JS ===============*/
 
+const contactForm=document.getElementById('contact-form'),
+     contactName=document.getElementById('contact-name'),
+     contactEmail=document.getElementById('contact-email'),
+     contactMsg=document.getElementById('contact-msg'),
+     contactMessage=document.getElementById('contact-message');
+
+
+const sendEmail=(e)=>{
+    e.preventDefault()
+    if(contactName.value==='' || contactEmail.value==='' || contactMsg.value=='')
+    {
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('colur-red')
+
+        contactMessage.textContent='write all the input feilds'
+    }
+    else{
+        emailjs.sendForm('service_v4ik6vc','template_fd5wqis','#contact-form','X1tHnV86ZFk29TfsL')
+        .then(()=>{
+            contactMessage.classList.add('color-blue')
+            contactMessage.textContent='Message sent'
+
+
+            setTimeout(()=>{
+                contactMessage.textContent=''
+            },5000)
+        },(error)=>{
+            alert('OoPs! something has failed...',error)
+        }
+        )
+        contactName.value=''
+        contactEmail.value=''
+        contactMsg.value=''
+    }
+}
+contactForm.addEventListener('submit',sendEmail)
+
+
+
+
+
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
